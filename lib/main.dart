167b2370f2c8ff2352cec40bf3e4d4aca680a204
MD Wahid Bappy <wahidbappy253@gmail.com>
2024-10-31 00:32:16 +0600

    code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,108 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(const DemoApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class DemoApp extends StatelessWidget {
-  const DemoApp({super.key});
-
-  MySnackBar(message, context) {
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message)),
-    );
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Demo App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'AppBar',
-            style: TextStyle(color: Colors.white),
-          ),
-          titleSpacing: 50,
-          //centerTitle: true,
-          backgroundColor: Colors.blue,
-          elevation: 10,
-          toolbarOpacity: 1,
-          actions: [
-            IconButton(
+      title: "MyApp",
+      home: Text('Home'),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
               onPressed: () {
-                MySnackBar('This is add Button', context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Profile(),
+                  ),
+                );
               },
-              icon: const Icon(Icons.add, color: Colors.white),
+              child: const Text('Go to Profile'),
             ),
-            IconButton(
+            ElevatedButton(
               onPressed: () {
-                MySnackBar('This is search Button', context);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Profile(),
+                  ),
+                );
               },
-              icon: const Icon(Icons.search, color: Colors.white),
-            ),
-            IconButton(
-              onPressed: () {
-                MySnackBar('This is settings Button', context);
-              },
-              icon: const Icon(Icons.settings, color: Colors.white),
-            ),
-            IconButton(
-              onPressed: () {
-                MySnackBar('This is comments Button', context);
-              },
-              icon: const Icon(Icons.pending_rounded, color: Colors.white),
+              child: const Text('Go to Settings'),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Back to Home'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Home()),
+                  (predicate) => false);
+            },
+            child: const Text('Back to Home'),
+          ),
+        ],
       ),
     );
   }
